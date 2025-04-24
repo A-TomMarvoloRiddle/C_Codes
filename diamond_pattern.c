f092e5be64f9e5de58a3b7a4f248c7cf40016a22
A-TomMarvoloRiddle <ap1203m@gmail.com>
2025-04-24 21:56:55 +0530

    Print Diamond pattern

---
+++ diamond_pattern.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() {
+int n,space; 
+printf("Enter the number of rows: ");
+scanf("%d",&n);
+space=n;
+// Run loop till the number of rows 
+for (int i = 0; i < n; i++) { 
+    // Loop for the initially space before the star printing 
+    for (int j = 0; j < space; j++) 
+        printf(" "); 
+    // Print the i+1 stars 
+    for (int j = 0; j <= i; j++) 
+        printf("* "); 
+    printf("\n"); 
+    space--;
+}
+space=1;
+for (int i = n; i > 0; i--) { 
+    // Loop for the initially space before the star printing 
+    for (int j = 0; j < space; j++) 
+        printf(" "); 
+    // Print the i+1 stars 
+    for (int j = 0; j < i; j++) 
+        printf("* "); 
+    printf("\n"); 
+    space++;
+}
+return 0;
+}
